29dea528c3e74fc6b2eb173b5a8e9ef2bd18608e
Leo <leonardhawkesjr@gmail.com>
2025-08-02 23:48:55 -0400

    Dummy PR to trigger netlify

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
     const projects = [
         {
             title: 'VibeLink',
-            subtitle: 'Share the Vibe, Any Platform',
+            subtitle: '🎵 Share the Vibe, Any Platform 🎵',
             imageKey: 'vibeLink',
             link: 'https://www.getvibelink.com',
             iosDownloadLink: 'https://apps.apple.com/us/app/vibelink-app/id6748924820',
