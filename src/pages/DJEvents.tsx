d748dcf59b820ef998ddf960e64d85918b0ccb2f
Leo <leonardhawkesjr@gmail.com>
2026-01-27 17:08:47 -0500

    Remove unused import

--- src/pages/DJEvents.tsx
+++ src/pages/DJEvents.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import { Link } from "react-router-dom";
 import SoundCloudEmbed from "../components/SoundCloudEmbed";
 import AudioVisualizer from "../components/AudioVisualizer";
 import DJBackground from "../components/DJBackground";
